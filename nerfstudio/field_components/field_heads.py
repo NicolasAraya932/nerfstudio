6a168b8cefcf2024e263da886aca7f57e8ba633a
Nicolas <nicoarayapcap@gmail.com>
2025-02-16 19:52:17 -0300

    Essential Files for FruitNeRF

--- nerfstudio/field_components/field_heads.py
+++ nerfstudio/field_components/field_heads.py
@@ -183,8 +183,8 @@ class SemanticFieldHead(FieldHead):
         activation: output head activation
     """
 
-    def __init__(self, num_classes: int, in_dim: Optional[int] = None) -> None:
-        super().__init__(in_dim=in_dim, out_dim=num_classes, field_head_name=FieldHeadNames.SEMANTICS, activation=None)
+    def __init__(self, num_classes: int, in_dim: Optional[int] = None, activation=None) -> None:
+        super().__init__(in_dim=in_dim, out_dim=num_classes, field_head_name=FieldHeadNames.SEMANTICS, activation=activation)
 
 
 class PredNormalsFieldHead(FieldHead):
