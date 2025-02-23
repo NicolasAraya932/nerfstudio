3f4bd6f7f52789d2f8c3ef82866b2ec05ea3620a
Nicolas <nicoarayapcap@gmail.com>
2025-02-22 23:13:29 -0300

    Solving FruitNeRF issues

--- nerfstudio/configs/method_configs.py
+++ nerfstudio/configs/method_configs.py
@@ -785,11 +785,7 @@ method_configs["fruit_nerf"] = TrainerConfig(
             dataparser=FruitNerfDataParserConfig(),
             train_num_rays_per_batch=4096,
             eval_num_rays_per_batch=4096,
-            camera_optimizer=CameraOptimizerConfig(
-                mode="SO3xR3",
-                optimizer=AdamOptimizerConfig(lr=6e-4, eps=1e-8, weight_decay=1e-2),
-                scheduler=ExponentialDecaySchedulerConfig(lr_final=6e-6, max_steps=200000),
-            ),
+            camera_optimizer=CameraOptimizerConfig(mode="SO3xR3"),
         ),
         model=FruitNerfModelConfig(eval_num_rays_per_chunk=1 << 15),
     ),
@@ -802,6 +798,10 @@ method_configs["fruit_nerf"] = TrainerConfig(
             "optimizer": AdamOptimizerConfig(lr=1e-2, eps=1e-15),
             "scheduler": ExponentialDecaySchedulerConfig(lr_final=0.0001, max_steps=200000),
         },
+        "camera_opt":{
+            "optimizer": AdamOptimizerConfig(lr=6e-4, eps=1e-8, weight_decay=1e-2),
+            "scheduler": ExponentialDecaySchedulerConfig(lr_final=6e-6, max_steps=200000),
+        }
     },
     viewer=ViewerConfig(num_rays_per_chunk=1 << 13),
     vis="viewer"
