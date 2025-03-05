dc575450b6f62bbea6472eb6c96e24e968bfbc38
Nicolas <nicoarayapcap@gmail.com>
2025-03-05 12:22:46 -0300

    fruit_nerf method_configs

--- nerfstudio/configs/method_configs.py
+++ nerfstudio/configs/method_configs.py
@@ -774,6 +774,40 @@ method_configs["splatfacto-mcmc"] = TrainerConfig(
     vis="viewer",
 )
 
+method_configs["fruit_nerf"] = TrainerConfig(
+    method_name="fruit_nerf",
+    steps_per_eval_batch=500,
+    steps_per_save=2000,
+    max_num_iterations=30000,
+    mixed_precision=True,
+    pipeline=VanillaPipelineConfig(
+        datamanager=FruitDataManagerConfig(
+            dataparser=FruitNerfDataParserConfig(),
+            train_num_rays_per_batch=4096,
+            eval_num_rays_per_batch=4096,
+        ),
+        model=FruitNerfModelConfig(
+            eval_num_rays_per_chunk=1 << 15,
+            camera_optimizer=CameraOptimizerConfig(mode="SO3xR3")),
+    ),
+    optimizers={
+        "proposal_networks": {
+            "optimizer": AdamOptimizerConfig(lr=1e-2, eps=1e-15),
+            "scheduler": ExponentialDecaySchedulerConfig(lr_final=0.0001, max_steps=200000),
+        },
+        "fields": {
+            "optimizer": AdamOptimizerConfig(lr=1e-2, eps=1e-15),
+            "scheduler": ExponentialDecaySchedulerConfig(lr_final=0.0001, max_steps=200000),
+        },
+        "camera_opt":{
+            "optimizer": AdamOptimizerConfig(lr=6e-4, eps=1e-8, weight_decay=1e-2),
+            "scheduler": ExponentialDecaySchedulerConfig(lr_final=6e-6, max_steps=200000),
+        }
+    },
+    viewer=ViewerConfig(num_rays_per_chunk=1 << 13),
+    vis="viewer"
+)
+
 def merge_methods(methods, method_descriptions, new_methods, new_descriptions, overwrite=True):
     """Merge new methods and descriptions into existing methods and descriptions.
     Args:
